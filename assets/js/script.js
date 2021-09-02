a395f5b157145d47eaa10972b6c88a8f4fd9e9bc
Bryan Daniel <brdaniel117@gmail.com>
2021-09-01 20:58:47 -0700

    created new dir and new js quiz to seperate code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,38 +7,9 @@ var completedTest = document.getElementById('completedTest');
 var currentQuestion = 0;
 var timeLeft = 60;
 var score = 0;
-var questions = [
-    { q: "With the HTML DOM, JavaScript can access and change all the _________ of an HTML document.", 
-    answer1: objects,
-    answer2: array,
-    answer3: elements,
-    answer4: blimps,
-    a: 3 },
-    { q: "In Javascript onselect is used for accessing button ______.",
-    answer1: feature,
-    answer2: looks,
-    answer3: buttons,
-    answer4: clicks,
-    a: 4 },
-    { q: "The clearTimeout() method _____ the execution of the function specified in setTimeout().",
-    answer1: stops,
-    answer2: starts,
-    answer3: skips,
-    answer4: repeat, 
-    a: 1 },
-    { q: "The _____ web storage can be used to store and retrieve data",
-    answer1: home,
-    answer2: local,
-    answer3: resident,
-    answer4: foreign, 
-    a: 2 },
-    { q: "In Javascript the === operator _______ data being logged in", 
-    answer1: ignores,
-    answer2: duplicates,
-    answer3: compares,
-    answer4: deletes,
-    a: 1 }
-]
+
+
+
 
 startButton.addEventListener('click', startQuiz);
 trueButton.addEventListener('click', () => answerQuestion(true));
@@ -73,11 +44,11 @@ function startTimer() {
 function answerQuestion(answer) {
 
     if (currentQuestion <= questions.length && questions[currentQuestion].a === answer) {
-        score ++;
+        score++;
     } else {
-    timeLeft -= 5;
+        timeLeft -= 5;
     }
-    currentQuestion ++;
+    currentQuestion++;
     displayQuestion();
 }
 
