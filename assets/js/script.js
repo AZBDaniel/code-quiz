9c18c93a999d23073484d8e4fdd5f4726e33c5eb
Bryan Daniel <brdaniel117@gmail.com>
2021-09-02 10:46:09 -0700

    foreach not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,32 @@
 //links var startButton to element id start-btn
 const startButton = document.getElementById('start-btn');
+
 //links var nextButton to element id next-btn
 const nextButton = document.getElementById('next-btn');
+
 //links var questionConElement to element id question-container
 const questionConElement = document.getElementById('questions-container');
-//
+
+//links var questionElement to element id question
 const questionElement = document.getElementById('question')
-//
+
+//links ansButtonElement to element answer-button
 const ansButtonElement = document.getElementById('answer-buttons')
+
 //create a var currentQuestions sets it to undefined
 let currentQuestion;
 //create a var shuffleQuestions sets it to undefined
 let shuffleQuestions;
 
 
-
+//start and next button event listener
 startButton.addEventListener('click', startQuiz);
+nextButton.addEventListener('click', () => {
+    currentQuestion++
+    setNextQuestion()
+})
 
+//start quiz function
 function startQuiz() {
     console.log('quiz started');
     //hides start button
@@ -37,9 +47,10 @@ function nextQuestion() {
     displayQuestions(shuffleQuestions[currentQuestion])
 }
 
+//display current question
 function displayQuestions(question) {
     questionElement.innerText = question.question
-    question.answers.forEach(answer => {
+    question.answers.forEach( answer => {
         const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
@@ -51,13 +62,16 @@ function displayQuestions(question) {
     })
 }
 
+//function to hide next button 
 function defaultState() {
+    removeStatusClass(document.body)
     nextButton.classList.add('hide')
     while (ansButtonElement.firstChild) {
         ansButtonElement.removeChild(ansButtonElement.firstChild)
     }
 }
 
+//function for election answer in array which unhides buttons
 function pickAnswer(e) {
     const choosenButton = e.target
     const correct = selectedButton.dataset.correct4
@@ -66,7 +80,31 @@ function pickAnswer(e) {
     Array.from(ansButtonElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
+    if (shuffleQuestions.length > currentQuestion + 1){
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+    
 }
+
+//set the new element class for correct or incorrect selected btn
+function compelStatusClass(element, correct) {
+    removeStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('incorrect')
+    }
+}
+
+//remove the elment class from correct or incorrect seleted btn
+function removeStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
+} 
+
 
 const questions = [
     //question 1
